e7417c2374d3feb54fde64e0697f611e81d9423a
devon <devonmcb@yahoo.com>
2016-03-22 00:28:07 -0400

    Built functions to update each stock in history collection.  Intended for a stand-alone php cron job.

--- examples_testing/testHistoryFromYQLtoMongo.php
+++ examples_testing/testHistoryFromYQLtoMongo.php
@@ -6,78 +6,20 @@
  * Time: 12:35 AM
  */
 
-
 // .. hardcoded for examples_testing directory
 require_once('../YQL_forTesting.php');
 require_once("../global.php");
-$yql = new YQL_forTesting();
-
-$symbol = "fb";
-
-
-class MongoSingleton{
-
-    /**************************************************************************
-    Data members
-     **************************************************************************/
-    private static $dbconn;
-
-    /**
-     * Protected constructor to prevent creating a new instance of the
-     * *Singleton* via the `new` operator from outside of this class.
-     */
-    protected function __construct()
-    {
-        $dbconn = new MongoClient();
-        $db = $dbconn->selectDB("test");
-    }
-
-    /**
-     * Returns the *Singleton* instance of this class.
-     *
-     * @return Singleton The *Singleton* instance.
-     */
-    public static function getInstance()
-    {
-        if (null === static::$dbconn) {
-            echo "it's a new Mongo connection.<br>";
-            static::$dbconn = new static();
-        }
-
-        echo "it's a re-used Mongo connection.<br>";
-        return static::$dbconn;
-    }
-
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * *Singleton* instance.
-     *
-     * @return void
-     */
-    private function __clone()
-    {
-    }
-
-    /**
-     * Private unserialize method to prevent unserializing of the *Singleton*
-     * instance.
-     *
-     * @return void
-     */
-    private function __wakeup()
-    {
-    }
-}
-
-
-
 
 
 /**Open PostgreSQL connection
  * loop through given JSON result passed as argument
  * saving each date's data in a separate query to PSQL
  */
-function addNewToPostgreSQL(&$yql,$symbol){
+function addNewToPostgreSQL($symbol){
+
+
+    $yql = new YQL_forTesting();
+
     //a very hard-coded db connection to PostgreSQL
     $dbconn = pg_connect("host="	 .DB_HOST
         ." dbname="	 .DATABASE
@@ -127,16 +69,66 @@ function addNewToPostgreSQL(&$yql,$symbol){
 
 }
 /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
-//addNewToPostgreSQL($yql,$symbol);
+//addNewToPostgreSQL($symbol);
 
 
+/**
+ * Fetch quotes from YQL for the given list of symbols. Return an associative
+ * array of arrays. The key is stock symbols (obviously unique), the value is
+ * an array being the fields of the quote (including redundantly the symbol).
+ *
+ * Rationale for making it this way: I need to merge the user-specific data of
+ * stock purchases with this current stock data, and being able to access current
+ * data with an efficiency of 1 is optimal: e.g., scenario: many records of
+ * purchases of the same stock; access current data of that stock reliably.
+ *
+ * @param String $symbolsString  a YQL-acceptible string of symbols to search for
+ *
+ * @return array $newArray An associative array of arrays of stock quotes, keyed by symbol
+ */
+function fetchManyFromYQL($symbolsString){
+    //echo $symbolsString;
+    // set up YQL connection to get current stock info
+    $y = new YQL_forTesting;
+
+    // NOTE: This YQL class receives JSON from YQL query, but converts the JSON
+    // into a PHP variable (a multi-dimensional array), so it's easy to weedle out
+    // pieces you need with nice PHP operators.
+    $resultArray = $y->getQuote( $symbolsString );
+    //var_dump($resultArray);
+
+    if(array_key_exists(0, $resultArray) && $resultArray[0] === 0){ // 2nd test prolly not necessary
+        return $resultArray;
+    }
+
+    // YQL gives superfluous data, so just take what we want from the JSON object,
+    // and save in a local array (need to cast it from "stdClass Object" to array.
+    //$theArray = $resultArray->{'query'}->{'results'}->{'quote'};
+    $theArray = $resultArray['query']['results']['quote'];
+
+    $newArray = array();
+    // This is an Array of stdClass Objects, so each stdClass Object should
+    // be cast to an array, so we have a multidimensional array.
+    $len = count($theArray);
+    for ($i = 0; $i < $len; $i++){
+        $value = (array)$theArray[$i];
+        $key = $value["symbol"];
+        // make a map, using unique field 'symbol' as key
+        $newArray[$key] = $value;
+    }
+
+    //echo "finishing function: MongoToYQL_Adapter:fetchManyFromYQL<br>";
+    return $newArray;
+}
 
 
 /**Open MongoDB connection
  * loop through given JSON result passed as argument
  * saving each date's data in a separate query to Mongo
  */
-function addNewToMongo(&$yql,$symbol){
+function addNewToMongo($symbol){
+
+    $yql = new YQL_forTesting();
 
     // NOTE: This YQL class receives JSON from YQL query, but converts the JSON
     // into a PHP variable (a multi-dimensional array), so it's easy to weedle out
@@ -172,7 +164,7 @@ function addNewToMongo(&$yql,$symbol){
 
         //testing: just print count of days in that year to show that something happened (if not showing data for each day)
         $index = $json['query']['count'] - 1;
-        echo "$index<br>";
+        echo "days in that year: $index<br>";
 
         //foreach ($json->query->results->quote as $Q){
         for($index; $index >= 0; $index--){
@@ -205,7 +197,7 @@ function addNewToMongo(&$yql,$symbol){
         $theHistory['lastday'] = $lastday;
     }
 
-    echo "<pre>"; var_dump($theHistory); echo "</pre>";
+    //echo "<pre>"; var_dump($theHistory); echo "</pre>";
 
     /* The PHP MongoDB Driver accepts only PHP arrays for inserts and queries
      * (see here: http://www.php.net/manual/en/mongo.queries.php)
@@ -214,11 +206,103 @@ function addNewToMongo(&$yql,$symbol){
 
 }
 /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
-//addNewToMongo($yql,$symbol);
-// ---------------------to clean up after that test if necessary-------------
-//notInHistory($symbol);
-//removeFromMongo($symbol);
-//notInHistory($symbol);
+//addNewToMongo("fb");
+//addNewToMongo("aapl");
+//addNewToMongo("ibm");
+//// ---------------------to clean up after that test if necessary-------------
+////notInHistory($symbol);
+////removeFromMongo($symbol);
+//notInHistory("fb");
+//notInHistory("aapl");
+//notInHistory("ibm");
+//notInHistory("zx");
+
+
+
+function getSymbolsFromHistory(){
+
+    $dbconn = new MongoClient();
+    $db = $dbconn->selectDB("test");
+    $collection = $db->history;
+
+    $result = array();
+
+    foreach ($collection->find(array(),array("symbol" => 1)) as $document) {
+        $result[] = $document["symbol"];
+    }
+    return $result;
+}
+/* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
+//$stocklist = getSymbolsFromHistory();
+//echo "<pre>"; var_dump($stocklist); echo "</pre>";
+
+
+
+function getQuotesFromYQL($stocklist){
+
+    $yql = new YQL_forTesting();
+
+    // build the string of stocks to feed to the YQL query function
+    $quotecommaquote = "%22,%22";
+    $thelist = array_shift($stocklist);
+    foreach($stocklist as $s){
+        $thelist .= $quotecommaquote . $s;
+    }
+
+    return fetchManyFromYQL($thelist);
+}
+/* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
+//$stocklist = getSymbolsFromHistory();
+//$quotes = getQuotesFromYQL($stocklist);
+//echo "<pre>"; var_dump($quotes); echo "</pre>";
+
+function dailyHistoryUpdate(){
+
+    $dbconn = new MongoClient();
+    $db = $dbconn->selectDB("test");
+    $collection = $db->history;
+
+    $stocklist = getSymbolsFromHistory();
+    $quotes = getQuotesFromYQL($stocklist);
+
+    foreach($quotes as $q){
+        $tempDay = array();
+        $tempDay["date"] = date('Y-m-d');//YQL quote does not give date
+                                                        /**********remove after done testing:
+                                                         * ********remove after done testing:
+                                                         * ********remove after done testing:
+                                                         * ********remove after done testing: */
+                                                        //$tempDay["date"] = '2016-03-24';
+        $tempDay["volume"] = $q["AverageDailyVolume"];
+        $tempDay["closingprice"] = $q["LastTradePriceOnly"];
+
+        $collection->update(
+            array("symbol" => $q["symbol"]),
+            array('$push' => array("day" => $tempDay) )  // adds the daily quote array to the end of the 'day' array
+        );
+        $collection->update(
+            array("symbol" => $q["symbol"]),
+            array('$set' => array("lastday" => $tempDay) ) // clobbers existing 'lastday' array
+        );
+    }
+}
+dailyHistoryUpdate();
+
+
+
+/******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ************************************************     place holder         ****************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ * ******************************************************************************************************************************************
+ */
 
 
 
@@ -238,10 +322,7 @@ function notInHistory($symbol){
     } else {
         echo "Data Doesn't Exist.<br>";
     }
-///////////////////////////////////////////////////////////////////////////////////////////should return TRUE or FALSE
-
 }
-
 /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
 //notInHistory("ibm");
 //notInHistory("lng");
@@ -249,34 +330,28 @@ function notInHistory($symbol){
 
 
 function getLastDate($symbol){
-
     $dbconn = new MongoClient();
     $db = $dbconn->selectDB("test");
     $collection = $db->history;
-
-
-    // SELECT a,b FROM users WHERE age=33
-    // $db->users->find(array("age" => 33), array("a" => 1, "b" => 1));
 
     $findThis = array('symbol' => $symbol);
     $returnThis = array("lastday" => 1);
 
     $doc = $collection->findOne($findThis,$returnThis);
     // INTERESTING: find returns a cursor, findOne returns a nice array.
-    // So, you can't manipulate a cursor directly, but you can an array.
 
     if(empty($doc)) {
         echo "That stock doesn't exist";
     } else {
-//        echo "Here's the var-dump: <br>";
-//        echo "<pre>"; var_dump($doc["lastday"]["date"]); echo "</pre>";
-//        echo "last date is: " . $doc["lastday"]["date"];
+        //        echo "<pre>"; var_dump($doc["lastday"]["date"]); echo "</pre>";
+        //        echo "last date is: " . $doc["lastday"]["date"];
         return $doc["lastday"]["date"];
     }
-
 }
 /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
-//getLastDate("lng");
+echo getLastDate("fb");
+
+
 
 
 function needsHistoricUpdate($symbol){
@@ -291,19 +366,13 @@ function needsHistoricUpdate($symbol){
     }
 
     echo "today is the " . date('N') . " day of the week.<br>";
-
     echo "the hour now is " . date("H") . "<br>";
     echo "the minute now is " . date("i") . "<br>";
 
-
-    //if(date('N')>5 || (date("H")>16 && date("i">30)))
     $date1 = new DateTime($lastdate);
     $date2 = new DateTime(date('Y-m-d'));
     $diff = $date2->diff($date1)->format("%a");
     echo "$diff is the difference in days<br>";
-
-
-
 }
 /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
 //needsHistoricUpdate('fb');
@@ -354,114 +423,6 @@ function removeFromMongo($symbol){
 //notInHistory($symbol);
 //removeFromMongo($symbol);
 //notInHistory($symbol);
-
-
-
-
-
-
-
-
-/**Open MongoDB connection
- * loop through given JSON result passed as argument
- * saving each date's data in a separate query to Mongo
- */
-function bringHistoryCurrentInMongo(&$yql,$symbol){
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-//THIS IS A COPY OF addToMongo() THAT HASN'T BEEN MODIFIED YET.
-    // NOTE: This YQL class receives JSON from YQL query, but converts the JSON
-    // into a PHP variable (a multi-dimensional array), so it's easy to weedle out
-    // pieces you need with nice PHP operators.
-    $resultArray = $yql->populateHistoricalData($symbol);
-    //echo "<pre>"; var_dump($resultArray); echo "</pre>";
-    /* WATCH OUT!  This was formed by querying YQL several times (once for each year), so
-       it contains a separate multi-dimensional array for each year. You'll have to
-       foreach loop through each year to weedle out the contiguous info you want.
-     */
-
-    /****************************************************************************
-     *  SET UP DATABASE CONNECTION
-     *****************************************************************************/
-    /* NOTE: YQL class receives JSON from YQL query, but converts the JSON
-     * into a PHP variable (a multi-dimensional array), so it's easy to weedle out
-     * pieces you need with nice PHP operators.
-     */
-    $dbconn = new MongoClient();
-    $db = $dbconn->selectDB("test");
-    $collection = $db->history;
-
-    /* YQL gives superfluous data, so just take what we want from the JSON object,
-     * and save in a local array (need to cast it from "stdClass Object" to array.
-     */
-    $theHistory['symbol'] = $symbol;
-    $theHistory['day'] = array(); // to hold each day's data
-
-    /* This damned thing takes like 60 seconds to run. */
-    // about 9 JSON elements in that array (representing whole years worth of quotes)
-    /* MongoDB version of capturing many years worth of a stock's price */
-    foreach($resultArray as $json){
-
-        //testing: just print count of days in that year to show that something happened (if not showing data for each day)
-        $index = $json['query']['count'] - 1;
-        echo "$index<br>";
-
-        //foreach ($json->query->results->quote as $Q){
-        for($index; $index >= 0; $index--){
-
-            /* instead of making an insert query for each date like in PostgreSQL,
-               we instead make a huge JSON object, and shove that into the
-               MongoDB document store. So, if you query a symbol, you'll get the
-               whole shebang.
-             */
-            $day = array(); // make a new one each loop.
-
-            $day['date']         = $json['query']['results']['quote'][$index]['Date'];
-            $day['volume']       = $json['query']['results']['quote'][$index]['Volume'];
-            $day['closingprice'] = $json['query']['results']['quote'][$index]['Close'];
-            //$theHistory['day'] = $day; // since $theHistory['day'] is an array, I believe it will simply append $day to it.
-            array_push($theHistory['day'], $day); // I don't think we need this level of formality
-
-        }
-
-        // Have to add a special array to contain the data for the last day.
-        // Back-story: Mongo doesn't permit you to peek at the last element of an array.
-        // You can remove the last element, but you can't know what you removed
-        // without loading the entire document (which here is very large) into a local
-        // instance. But you can peek at a specially-made (what do you call it?) data member
-        // designed to hold the last element in the array in question. Bitches!
-        $lastday = array();
-        $lastday['date']         = $json['query']['results']['quote'][0]['Date'];
-        $lastday['volume']       = $json['query']['results']['quote'][0]['Volume'];
-        $lastday['closingprice'] = $json['query']['results']['quote'][0]['Close'];
-        $theHistory['lastday'] = $lastday;
-    }
-
-    echo "<pre>"; var_dump($theHistory); echo "</pre>";
-
-    /* The PHP MongoDB Driver accepts only PHP arrays for inserts and queries
-     * (see here: http://www.php.net/manual/en/mongo.queries.php)
-     */
-    $collection->insert($theHistory);
-
-}
-/* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  TEST TEST TEST TEST TEST TEST   TEST TEST TEST TEST TEST TEST   */
-//bringHistoryCurrentInMongo($yql,$symbol);
-// ---------------------to clean up after that test if necessary-------------
-//notInHistory($symbol);
-//removeFromMongo($symbol);
-//notInHistory($symbol);
-
-
-
-
 
 
 
